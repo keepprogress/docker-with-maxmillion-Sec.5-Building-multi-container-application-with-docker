7ad5b1408c2dce6a5065f6506c1adc7aaec25698
getzch <keepprogress@gmail.com>
2022-10-19 20:33:46 +0800

    Sec-5 .87 Volumes, Bind Mounts & Polishing for the NodeJS Container

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,13 @@
 # docker run --name goals-backend --rm -d -p 80:80 goals-node
 
-docker run --name goals-backend --rm -d --network goals-net goals-node
+# docker run --name goals-backend --rm -d --network goals-net goals-node
+
+# docker run --name goals-backend \
+# -v /Users/getzch/Documents/Projects/multi-01-starting-setup/backend:/app \
+# -v /app/node_modules -v logs:/app/logs \
+# --rm -d --network goals-net goals-node 
+
+docker run --name goals-backend \
+-v /Users/getzch/Documents/Projects/multi-01-starting-setup/backend:/app \
+-v /app/node_modules -v logs:/app/logs -e MONGODB_USERNAME=max \
+--rm -d --network goals-net goals-node 
--- runMogo.sh
+++ runMogo.sh
@@ -1,1 +1,7 @@
-doker run --name mogodb --rm -d -p 27017:27017 mongo
+# doker run --name mogodb --rm -d -p 27017:27017 mongo
+
+# docker run --name mongodb --rm -v data:/data/db -d -p 27017:27017 mongo
+
+docker run --name mongodb --rm -v data:/data/db -d -p 27017:27017 \
+-e MONGO_INITDB_ROOT_USERNAME=max \
+-e MONGO_INITDB_ROOT_PASSWORD=secret mongo
